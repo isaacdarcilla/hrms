4050cc36c8f256c07928c93d0a39a7cdf33d80ce
Isaac <isaacdarcilla@gmail.com>
2021-02-06 09:39:43 +0800

    refactor database

--- resources/js/Pages/Contacts/Edit.vue
+++ resources/js/Pages/Contacts/Edit.vue
@@ -1,37 +1,167 @@
 <template>
   <div>
     <h1 class="mb-8 font-bold text-3xl">
-      <inertia-link class="text-indigo-400 hover:text-indigo-600" :href="route('contacts')">Contacts</inertia-link>
+      <inertia-link
+        class="text-indigo-400 hover:text-indigo-600"
+        :href="route('employees')"
+        >Employees</inertia-link
+      >
       <span class="text-indigo-400 font-medium">/</span>
-      {{ form.first_name }} {{ form.last_name }}
+      {{ form.first_name }} {{ form.last_name }} 👥
     </h1>
     <trashed-message v-if="contact.deleted_at" class="mb-6" @restore="restore">
       This contact has been deleted.
     </trashed-message>
+    <div class="h-full">
+      <div class="border-b-2 block md:flex">
+        <div class="w-full md:w-2/5 p-4 sm:p-6 lg:p-8 rounded-xl bg-white shadow-md">
+          <figure class="bg-white rounded-xl p-8">
+            <img
+              class="w-32 h-32 rounded-full mx-auto"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+              alt=""
+              width="384"
+              height="512"
+            />
+            <div class="pt-6 text-center space-y-4">
+              <figcaption class="text-lg font-large font-semibold">
+                <div class="text-cyan-600">{{ contact.first_name }} {{ contact.last_name }}</div>
+                <div class="text-gray-500">{{ contact.email }}</div>
+              </figcaption>
+            </div>
+          </figure>
+        </div>
+
+        <div class="w-full md:w-3/5 p-8 lg:ml-4 rounded-xl bg-white shadow-md">
+          <div class="rounded p-3">
+            <div class="pb-6">
+              <label for="name" class="font-semibold text-gray-700 block pb-1"
+                >Name</label
+              >
+              <div class="flex">
+                <input
+                  disabled
+                  id="username"
+                  class="border-1 rounded-r px-4 py-2 w-full"
+                  type="text"
+                  value="Jane Name"
+                />
+              </div>
+            </div>
+            <div class="pb-4">
+              <label for="about" class="font-semibold text-gray-700 block pb-1"
+                >Email</label
+              >
+              <input
+                disabled
+                id="email"
+                class="border-1 rounded-r px-4 py-2 w-full"
+                type="email"
+                value="example@example.com"
+              />
+              <span class="text-gray-600 pt-4 block opacity-70"
+                >Personal login information of your account</span
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="bg-white rounded shadow overflow-hidden">
       <form @submit.prevent="submit">
         <div class="p-8 -mr-6 -mb-8 flex flex-wrap">
-          <text-input v-model="form.first_name" :error="errors.first_name" class="pr-6 pb-8 w-full lg:w-1/2" label="First name" />
-          <text-input v-model="form.last_name" :error="errors.last_name" class="pr-6 pb-8 w-full lg:w-1/2" label="Last name" />
-          <select-input v-model="form.organization_id" :error="errors.organization_id" class="pr-6 pb-8 w-full lg:w-1/2" label="Organization">
+          <text-input
+            v-model="form.first_name"
+            :error="errors.first_name"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="First name"
+          />
+          <text-input
+            v-model="form.last_name"
+            :error="errors.last_name"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Last name"
+          />
+          <select-input
+            v-model="form.organization_id"
+            :error="errors.organization_id"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Organization"
+          >
             <option :value="null" />
-            <option v-for="organization in organizations" :key="organization.id" :value="organization.id">{{ organization.name }}</option>
+            <option
+              v-for="organization in organizations"
+              :key="organization.id"
+              :value="organization.id"
+            >
+              {{ organization.name }}
+            </option>
           </select-input>
-          <text-input v-model="form.email" :error="errors.email" class="pr-6 pb-8 w-full lg:w-1/2" label="Email" />
-          <text-input v-model="form.phone" :error="errors.phone" class="pr-6 pb-8 w-full lg:w-1/2" label="Phone" />
-          <text-input v-model="form.address" :error="errors.address" class="pr-6 pb-8 w-full lg:w-1/2" label="Address" />
-          <text-input v-model="form.city" :error="errors.city" class="pr-6 pb-8 w-full lg:w-1/2" label="City" />
-          <text-input v-model="form.region" :error="errors.region" class="pr-6 pb-8 w-full lg:w-1/2" label="Province/State" />
-          <select-input v-model="form.country" :error="errors.country" class="pr-6 pb-8 w-full lg:w-1/2" label="Country">
+          <text-input
+            v-model="form.email"
+            :error="errors.email"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Email"
+          />
+          <text-input
+            v-model="form.phone"
+            :error="errors.phone"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Phone"
+          />
+          <text-input
+            v-model="form.address"
+            :error="errors.address"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Address"
+          />
+          <text-input
+            v-model="form.city"
+            :error="errors.city"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="City"
+          />
+          <text-input
+            v-model="form.region"
+            :error="errors.region"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Province/State"
+          />
+          <select-input
+            v-model="form.country"
+            :error="errors.country"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Country"
+          >
             <option :value="null" />
             <option value="CA">Canada</option>
             <option value="US">United States</option>
           </select-input>
-          <text-input v-model="form.postal_code" :error="errors.postal_code" class="pr-6 pb-8 w-full lg:w-1/2" label="Postal code" />
+          <text-input
+            v-model="form.postal_code"
+            :error="errors.postal_code"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Postal code"
+          />
         </div>
-        <div class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex items-center">
-          <button v-if="!contact.deleted_at" class="text-red-600 hover:underline" tabindex="-1" type="button" @click="destroy">Delete Contact</button>
-          <loading-button :loading="sending" class="btn-indigo ml-auto" type="submit">Update Contact</loading-button>
+        <div
+          class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex items-center"
+        >
+          <button
+            v-if="!contact.deleted_at"
+            class="text-red-600 hover:underline"
+            tabindex="-1"
+            type="button"
+            @click="destroy"
+          >
+            Delete Contact
+          </button>
+          <loading-button
+            :loading="sending"
+            class="btn-indigo ml-auto"
+            type="submit"
+            >Update Contact</loading-button
+          >
         </div>
       </form>
     </div>
@@ -39,17 +169,17 @@
 </template>
 
 <script>
-import Layout from '@/Shared/Layout'
-import LoadingButton from '@/Shared/LoadingButton'
-import SelectInput from '@/Shared/SelectInput'
-import TextInput from '@/Shared/TextInput'
-import TrashedMessage from '@/Shared/TrashedMessage'
+import Layout from "@/Shared/Layout";
+import LoadingButton from "@/Shared/LoadingButton";
+import SelectInput from "@/Shared/SelectInput";
+import TextInput from "@/Shared/TextInput";
+import TrashedMessage from "@/Shared/TrashedMessage";
 
 export default {
   metaInfo() {
     return {
       title: `${this.form.first_name} ${this.form.last_name}`,
-    }
+    };
   },
   layout: Layout,
   components: {
@@ -63,7 +193,7 @@ export default {
     contact: Object,
     organizations: Array,
   },
-  remember: 'form',
+  remember: "form",
   data() {
     return {
       sending: false,
@@ -79,25 +209,29 @@ export default {
         country: this.contact.country,
         postal_code: this.contact.postal_code,
       },
-    }
+    };
   },
   methods: {
     submit() {
-      this.$inertia.put(this.route('employees.update', this.contact.id), this.form, {
-        onStart: () => this.sending = true,
-        onFinish: () => this.sending = false,
-      })
+      this.$inertia.put(
+        this.route("employees.update", this.contact.id),
+        this.form,
+        {
+          onStart: () => (this.sending = true),
+          onFinish: () => (this.sending = false),
+        }
+      );
     },
     destroy() {
-      if (confirm('Are you sure you want to delete this contact?')) {
-        this.$inertia.delete(this.route('employees.destroy', this.contact.id))
+      if (confirm("Are you sure you want to delete this contact?")) {
+        this.$inertia.delete(this.route("employees.destroy", this.contact.id));
       }
     },
     restore() {
-      if (confirm('Are you sure you want to restore this contact?')) {
-        this.$inertia.put(this.route('employees.restore', this.contact.id))
+      if (confirm("Are you sure you want to restore this contact?")) {
+        this.$inertia.put(this.route("employees.restore", this.contact.id));
       }
     },
   },
-}
+};
 </script>
--- resources/js/Pages/Contacts/Index.vue
+++ resources/js/Pages/Contacts/Index.vue
@@ -2,7 +2,11 @@
   <div>
     <h1 class="mb-8 font-bold text-3xl">Employees 👥</h1>
     <div class="mb-6 flex justify-between items-center">
-      <search-filter v-model="form.search" class="w-full max-w-md mr-4" @reset="reset">
+      <search-filter
+        v-model="form.search"
+        class="w-full max-w-md mr-4"
+        @reset="reset"
+      >
         <label class="block text-gray-700">Trashed:</label>
         <select v-model="form.trashed" class="mt-1 w-full form-select">
           <option :value="null" />
@@ -19,21 +23,62 @@
       <table class="min-w-full divide-y divide-gray-200">
         <thead class="bg-white">
           <tr class="transition-all hover:bg-gray-100 hover:shadow-lg">
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Employee Name</th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Department</th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Position</th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Phone</th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
+            <th
+              scope="col"
+              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Employee Name
+            </th>
+            <th
+              scope="col"
+              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Department
+            </th>
+            <th
+              scope="col"
+              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Position
+            </th>
+            <th
+              scope="col"
+              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Phone
+            </th>
+            <th
+              scope="col"
+              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Status
+            </th>
+            <th
+              scope="col"
+              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Action
+            </th>
           </tr>
         </thead>
         <tbody class="bg-white divide-y divide-gray-200">
-          <tr v-for="contact in contacts.data" :key="contact.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
+          <tr
+            v-for="contact in contacts.data"
+            :key="contact.id"
+            class="hover:bg-gray-100 focus-within:bg-gray-100"
+          >
             <td class="border-t">
-              <inertia-link class="px-6 py-4 flex items-center focus:text-indigo-500" :href="route('employees.edit', contact.id)">
+              <inertia-link
+                class="px-6 py-4 flex items-center focus:text-indigo-500"
+                :href="route('employees.edit', contact.id)"
+              >
                 <div class="flex items-center">
                   <div class="flex-shrink-0 h-10 w-10">
-                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt="">
+                    <img
+                      class="h-10 w-10 rounded-full"
+                      src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+                      alt=""
+                    />
                   </div>
                   <div class="ml-4">
                     <div class="text-sm font-medium text-gray-900">
@@ -44,11 +89,19 @@
                     </div>
                   </div>
                 </div>
-                <icon v-if="contact.deleted_at" name="trash" class="flex-shrink-0 w-3 h-3 fill-gray-400 ml-2" />
+                <icon
+                  v-if="contact.deleted_at"
+                  name="trash"
+                  class="flex-shrink-0 w-3 h-3 fill-gray-400 ml-2"
+                />
               </inertia-link>
             </td>
             <td class="px-6 py-4 whitespace-nowrap">
-              <inertia-link class="px-6 py-4 whitespace-nowrap text-sm text-gray-900" :href="route('employees.edit', contact.id)" tabindex="-1">
+              <inertia-link
+                class="px-6 py-4 whitespace-nowrap text-sm text-gray-900"
+                :href="route('employees.edit', contact.id)"
+                tabindex="-1"
+              >
                 <div v-if="contact.organization">
                   {{ contact.organization.name }}
                 </div>
@@ -56,29 +109,45 @@
               </inertia-link>
             </td>
             <td class="border-t">
-              <inertia-link class="px-6 py-4 whitespace-nowrap text-sm text-gray-500" :href="route('employees.edit', contact.id)" tabindex="-1">
+              <inertia-link
+                class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+                :href="route('employees.edit', contact.id)"
+                tabindex="-1"
+              >
                 {{ contact.city }}
               </inertia-link>
             </td>
             <td class="border-t">
-              <inertia-link class="px-6 py-4 whitespace-nowrap text-sm text-gray-500" :href="route('employees.edit', contact.id)" tabindex="-1">
+              <inertia-link
+                class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+                :href="route('employees.edit', contact.id)"
+                tabindex="-1"
+              >
                 {{ contact.phone }}
               </inertia-link>
             </td>
             <td class="px-6 py-4 whitespace-nowrap">
-              <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-300 text-green-800">
-                      Active
+              <span
+                class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-300 text-green-800"
+              >
+                Active
               </span>
             </td>
             <td class="border-t w-px">
-              <inertia-link class="px-4 flex items-center" :href="route('employees.edit', contact.id)" tabindex="-1">
+              <inertia-link
+                class="px-4 flex items-center"
+                :href="route('employees.edit', contact.id)"
+                tabindex="-1"
+              >
                 👁️‍🗨️
               </inertia-link>
             </td>
           </tr>
         </tbody>
         <tr v-if="contacts.data.length === 0">
-          <td class="border-t px-6 py-4 font-bold" colspan="4">☹️ No employees found.</td>
+          <td class="border-t px-6 py-4 font-bold" colspan="4">
+            ☹️ No employees found.
+          </td>
         </tr>
       </table>
     </div>
@@ -87,16 +156,16 @@
 </template>
 
 <script>
-import Icon from '@/Shared/Icon'
-import Layout from '@/Shared/Layout'
-import mapValues from 'lodash/mapValues'
-import Pagination from '@/Shared/Pagination'
-import pickBy from 'lodash/pickBy'
-import SearchFilter from '@/Shared/SearchFilter'
-import throttle from 'lodash/throttle'
+import Icon from "@/Shared/Icon";
+import Layout from "@/Shared/Layout";
+import mapValues from "lodash/mapValues";
+import Pagination from "@/Shared/Pagination";
+import pickBy from "lodash/pickBy";
+import SearchFilter from "@/Shared/SearchFilter";
+import throttle from "lodash/throttle";
 
 export default {
-  metaInfo: { title: 'Employees' },
+  metaInfo: { title: "Employees" },
   layout: Layout,
   components: {
     Icon,
@@ -113,21 +182,26 @@ export default {
         search: this.filters.search,
         trashed: this.filters.trashed,
       },
-    }
+    };
   },
   watch: {
     form: {
-      handler: throttle(function() {
-        let query = pickBy(this.form)
-        this.$inertia.replace(this.route('employees', Object.keys(query).length ? query : { remember: 'forget' }))
+      handler: throttle(function () {
+        let query = pickBy(this.form);
+        this.$inertia.replace(
+          this.route(
+            "employees",
+            Object.keys(query).length ? query : { remember: "forget" }
+          )
+        );
       }, 150),
       deep: true,
     },
   },
   methods: {
     reset() {
-      this.form = mapValues(this.form, () => null)
+      this.form = mapValues(this.form, () => null);
     },
   },
-}
+};
 </script>
