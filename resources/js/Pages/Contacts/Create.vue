bf23b4295262bef92843cd3622035aab2bf34393
Isaac <isaacdarcilla@gmail.com>
2021-02-07 11:39:03 +0800

    refactor employee layout

--- resources/js/Pages/Contacts/Create.vue
+++ resources/js/Pages/Contacts/Create.vue
@@ -1,32 +1,96 @@
 <template>
   <div>
     <h1 class="mb-8 font-bold text-3xl">
-      <inertia-link class="text-indigo-400 hover:text-indigo-600" :href="route('employees')">Contacts</inertia-link>
+      <inertia-link
+        class="text-indigo-400 hover:text-indigo-600"
+        :href="route('employees')"
+        >Contacts</inertia-link
+      >
       <span class="text-indigo-400 font-medium">/</span> Create
     </h1>
     <div class="bg-white rounded shadow overflow-hidden">
       <form @submit.prevent="submit">
         <div class="p-8 -mr-6 -mb-8 flex flex-wrap">
-          <text-input v-model="form.first_name" :error="errors.first_name" class="pr-6 pb-8 w-full lg:w-1/2" label="First name" />
-          <text-input v-model="form.last_name" :error="errors.last_name" class="pr-6 pb-8 w-full lg:w-1/2" label="Last name" />
-          <select-input v-model="form.organization_id" :error="errors.organization_id" class="pr-6 pb-8 w-full lg:w-1/2" label="Organization">
+          <text-input
+            v-model="form.first_name"
+            :error="errors.first_name"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="First name"
+          />
+          <text-input
+            v-model="form.last_name"
+            :error="errors.last_name"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Last name"
+          />
+          <select-input
+            v-model="form.organization_id"
+            :error="errors.organization_id"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Organization"
+          >
             <option :value="null" />
-            <option v-for="organization in organizations" :key="organization.id" :value="organization.id">{{ organization.name }}</option>
+            <option
+              v-for="organization in organizations"
+              :key="organization.id"
+              :value="organization.id"
+            >
+              {{ organization.name }}
+            </option>
           </select-input>
-          <text-input v-model="form.email" :error="errors.email" class="pr-6 pb-8 w-full lg:w-1/2" label="Email" />
-          <text-input v-model="form.phone" :error="errors.phone" class="pr-6 pb-8 w-full lg:w-1/2" label="Phone" />
-          <text-input v-model="form.address" :error="errors.address" class="pr-6 pb-8 w-full lg:w-1/2" label="Address" />
-          <text-input v-model="form.city" :error="errors.city" class="pr-6 pb-8 w-full lg:w-1/2" label="City" />
-          <text-input v-model="form.region" :error="errors.region" class="pr-6 pb-8 w-full lg:w-1/2" label="Province/State" />
-          <select-input v-model="form.country" :error="errors.country" class="pr-6 pb-8 w-full lg:w-1/2" label="Country">
+          <text-input
+            v-model="form.email"
+            :error="errors.email"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Email"
+          />
+          <text-input
+            v-model="form.phone"
+            :error="errors.phone"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Phone"
+          />
+          <text-input
+            v-model="form.address"
+            :error="errors.address"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Address"
+          />
+          <text-input
+            v-model="form.city"
+            :error="errors.city"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="City"
+          />
+          <text-input
+            v-model="form.region"
+            :error="errors.region"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Province/State"
+          />
+          <select-input
+            v-model="form.country"
+            :error="errors.country"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Country"
+          >
             <option :value="null" />
             <option value="CA">Canada</option>
             <option value="US">United States</option>
           </select-input>
-          <text-input v-model="form.postal_code" :error="errors.postal_code" class="pr-6 pb-8 w-full lg:w-1/2" label="Postal code" />
+          <text-input
+            v-model="form.postal_code"
+            :error="errors.postal_code"
+            class="pr-6 pb-8 w-full lg:w-1/2"
+            label="Postal code"
+          />
         </div>
-        <div class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex justify-end items-center">
-          <loading-button :loading="sending" class="btn-indigo" type="submit">Create Contact</loading-button>
+        <div
+          class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex justify-end items-center"
+        >
+          <loading-button :loading="sending" class="btn-indigo" type="submit"
+            >Create Contact</loading-button
+          >
         </div>
       </form>
     </div>
@@ -34,13 +98,13 @@
 </template>
 
 <script>
-import Layout from '@/Shared/Layout'
-import LoadingButton from '@/Shared/LoadingButton'
-import SelectInput from '@/Shared/SelectInput'
-import TextInput from '@/Shared/TextInput'
+import Layout from "@/Shared/Layout";
+import LoadingButton from "@/Shared/LoadingButton";
+import SelectInput from "@/Shared/SelectInput";
+import TextInput from "@/Shared/TextInput";
 
 export default {
-  metaInfo: { title: 'Create Contact' },
+  metaInfo: { title: "Create Contact" },
   layout: Layout,
   components: {
     LoadingButton,
@@ -51,7 +115,7 @@ export default {
     errors: Object,
     organizations: Array,
   },
-  remember: 'form',
+  remember: "form",
   data() {
     return {
       sending: false,
@@ -67,15 +131,15 @@ export default {
         country: null,
         postal_code: null,
       },
-    }
+    };
   },
   methods: {
     submit() {
-      this.$inertia.post(this.route('employees.store'), this.form, {
-        onStart: () => this.sending = true,
-        onFinish: () => this.sending = false,
-      })
+      this.$inertia.post(this.route("employees.store"), this.form, {
+        onStart: () => (this.sending = true),
+        onFinish: () => (this.sending = false),
+      });
     },
   },
-}
+};
 </script>
