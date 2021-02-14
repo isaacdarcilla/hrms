9dabedeaa27c355bbc399c5a28836536b8e72d2c
Isaac <isaacdarcilla@gmail.com>
2021-02-14 09:34:19 +0800

    ref: login and scrollbar

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,34 +1,98 @@
 <template>
   <div class="p-6 bg-gray-100 min-h-screen flex justify-center items-center">
-    <div class="w-full max-w-md">
-      <!-- <logo class="block mx-auto w-full max-w-xs fill-white" height="50" /> -->
-      <form class="mt-8 bg-white rounded-lg shadow-xl overflow-hidden" @submit.prevent="submit">
-        <div class="px-10 py-12">
-          <h1 class="text-center font-bold text-3xl">CatSU–HRMS</h1>
-          <div class="mx-auto mt-6 w-24 border-b-2" />
-          <text-input v-model="form.email" :error="errors.email" class="mt-10" label="Email" type="text" autofocus autocapitalize="off" />
-          <text-input v-model="form.password" class="mt-6" label="Password" type="password" />
-          <label class="mt-6 select-none flex items-center" for="remember">
-            <input id="remember" name="comments" type="checkbox" class="mr-1 focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" v-model="form.remember">
-            <span class="text-sm">Remember login credentials</span>
-          </label>
+    <div
+      class="flex items-center min-h-screen p-4 bg-gray-100 lg:justify-center"
+    >
+      <div
+        class="flex flex-col overflow-hidden bg-white rounded-md shadow-lg max md:flex-row md:flex-1 lg:max-w-screen-md"
+      >
+        <div class="p-4 py-6 text-white bg-indigo-600">
+          <div class="justify-center py-4 content-center">
+              <img class="mx-auto object-center" src="/img/user.png" width="80">
+          </div>
+          <div class="my-3 text-4xl font-bold tracking-wider text-center">
+            <a>CatSU HRMS</a>
+          </div>
+          <p class="mt-6 font-normal text-center text-gray-300 md:mt-0">
+            Catanduanes State University Human Resource Management System
+          </p>
+          <p
+            class="flex flex-col items-center justify-center mt-10 text-center"
+          >
+            <span>Login to get started.</span>
+          </p>
         </div>
-        <div class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex justify-between items-center">
-          <a class="hover:underline" tabindex="-1"></a>
-          <loading-button :loading="sending" class="btn-indigo" type="submit">Login</loading-button>
+        <div class="p-5 bg-white md:flex-1">
+          <h3 class="my-4 mx-5 text-2xl font-semibold text-gray-700">
+            Account Login
+          </h3>
+          <form
+            @submit.prevent="submit"
+            class="flex flex-col space-y-5 mx-5 pb-5"
+          >
+            <div class="flex flex-col space-y-1">
+              <label for="email" class="text-sm font-semibold text-gray-500"
+                >Email address</label
+              >
+              <text-input
+                v-model="form.email"
+                :error="errors.email"
+                type="text"
+                autofocus
+                placeholder="Enter your email address"
+                autocapitalize="off"
+                class="px-0 py-2 transition duration-300 border-0 border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+              />
+            </div>
+            <div class="flex flex-col space-y-1">
+              <div class="flex items-center justify-between">
+                <label
+                  for="password"
+                  class="text-sm font-semibold text-gray-500"
+                  >Password</label
+                >
+              </div>
+              <input
+                v-model="form.password"
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                class="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+              />
+            </div>
+            <div class="flex items-center space-x-2">
+              <input
+                v-model="form.remember"
+                type="checkbox"
+                id="remember"
+                class="w-4 h-4 transition duration-300 rounded focus:ring-2 focus:ring-offset-0 focus:outline-none focus:ring-blue-200"
+              />
+              <label for="remember" class="text-sm font-semibold text-gray-900"
+                >Remember login credentials</label
+              >
+            </div>
+            <div class="items-center text-right">
+              <loading-button
+                :loading="sending"
+                class="btn-indigo"
+                type="submit"
+                >Login</loading-button
+              >
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import LoadingButton from '@/Shared/LoadingButton'
-import Logo from '@/Shared/Logo'
-import TextInput from '@/Shared/TextInput'
+import LoadingButton from "@/Shared/LoadingButton";
+import Logo from "@/Shared/Logo";
+import TextInput from "@/Shared/TextInput";
 
 export default {
-  metaInfo: { title: 'Login' },
+  metaInfo: { title: "Login" },
   components: {
     LoadingButton,
     Logo,
@@ -41,11 +105,11 @@ export default {
     return {
       sending: false,
       form: {
-        email: '',
-        password: '',
+        email: "",
+        password: "",
         remember: null,
       },
-    }
+    };
   },
   methods: {
     submit() {
@@ -53,13 +117,13 @@ export default {
         email: this.form.email,
         password: this.form.password,
         remember: this.form.remember,
-      }
+      };
 
-      this.$inertia.post(this.route('login.attempt'), data, {
-        onStart: () => this.sending = true,
-        onFinish: () => this.sending = false,
-      })
+      this.$inertia.post(this.route("login.attempt"), data, {
+        onStart: () => (this.sending = true),
+        onFinish: () => (this.sending = false),
+      });
     },
   },
-}
+};
 </script>
