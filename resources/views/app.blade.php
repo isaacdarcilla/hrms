02040c8fed17f03fd200b9a570a10d28c9aaee38
Isaac <isaacdarcilla@gmail.com>
2021-02-14 10:56:12 +0800

    ref: added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link rel="icon" type="image/png" href="/img/user.png"/>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 
     {{-- Inertia --}}
