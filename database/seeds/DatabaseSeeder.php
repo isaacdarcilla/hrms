3957c4b7ca5a935cabd2dcb5edd461b30b911757
Isaac <isaacdarcilla@gmail.com>
2021-02-18 05:59:59 +0700

    ref: seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,16 +19,5 @@ class DatabaseSeeder extends Seeder
             'email' => 'isaacdarcilla@gmail.com',
             'owner' => true,
         ]);
-
-        factory(User::class, 5)->create(['account_id' => $account->id]);
-
-        $organizations = factory(Organization::class, 100)
-            ->create(['account_id' => $account->id]);
-
-        factory(Contact::class, 100)
-            ->create(['account_id' => $account->id])
-            ->each(function ($contact) use ($organizations) {
-                $contact->update(['organization_id' => $organizations->random()->id]);
-            });
     }
 }
