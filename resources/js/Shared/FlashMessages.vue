e49507c1906542067b5b0884066001cd669d078d
Isaac <isaacdarcilla@gmail.com>
2021-03-02 09:06:55 +0800

    ref: flash messages

--- resources/js/Shared/FlashMessages.vue
+++ resources/js/Shared/FlashMessages.vue
@@ -2,47 +2,8 @@
   <div>
     <div
       v-if="$page.flash.success && show"
-      class="mb-8 flex items-center justify-between bg-green-500 rounded-lg"
+      class="mb-8 flex items-center justify-between bg-green-400 rounded-lg"
     >
-      <!-- <div
-        class="absolute flex max-w-xs w-full mt-4 r-4 top-0 right-0 bg-green-500 rounded-lg shadow p-4"
-      >
-        <div class="mr-2">
-          <svg
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 13l4 4L19 7"
-            ></path>
-          </svg>
-        </div>
-        <div class="flex-1 text-gray-800">
-          {{ $page.flash.success }}
-        </div>
-        <div class="ml-2">
-          <button type="button" class="group mr-2 p-2" @click="show = false">
-            <svg
-              class="block w-2 h-2 fill-white group-hover:fill-green-800"
-              xmlns="http://www.w3.org/2000/svg"
-              width="235.908"
-              height="235.908"
-              viewBox="278.046 126.846 235.908 235.908"
-            >
-              <path
-                d="M506.784 134.017c-9.56-9.56-25.06-9.56-34.62 0L396 210.18l-76.164-76.164c-9.56-9.56-25.06-9.56-34.62 0-9.56 9.56-9.56 25.06 0 34.62L361.38 244.8l-76.164 76.165c-9.56 9.56-9.56 25.06 0 34.62 9.56 9.56 25.06 9.56 34.62 0L396 279.42l76.164 76.165c9.56 9.56 25.06 9.56 34.62 0 9.56-9.56 9.56-25.06 0-34.62L430.62 244.8l76.164-76.163c9.56-9.56 9.56-25.06 0-34.62z"
-              />
-            </svg>
-          </button>
-        </div>
-      </div> -->
-
       <div class="flex items-center">
         <svg
           class="ml-4 mr-2 flex-shrink-0 w-4 h-4 fill-white"
@@ -71,7 +32,7 @@
     </div>
     <div
       v-if="($page.flash.error || Object.keys($page.errors).length > 0) && show"
-      class="mb-8 flex items-center justify-between bg-red-500 rounded-lg"
+      class="mb-8 flex items-center justify-between bg-red-400 rounded-lg"
     >
       <div class="flex items-center">
         <svg
