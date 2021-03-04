193720d9bdacd8639a4e45e1a5c07a99e1b4b307
Isaac <isaacdarcilla@gmail.com>
2021-03-04 10:54:30 +0800

    chore: data sheet family

--- resources/js/Pages/Sheet/DataSheet.vue
+++ resources/js/Pages/Sheet/DataSheet.vue
@@ -51,206 +51,11 @@
 
         <personal />
 
+        <family />
+
         <div class="border-black border-b-2 bg-gray-500">
           <div class="p-1 italic font-semibold text-xs text-white">
-            II. FAMILY BACKGROUND
-          </div>
-        </div>
-        <div class="flex">
-          <div class="w-3/12 border-b-2 border-black bg-gray-300">
-            <div class="flex h-6">
-              <div class="my-auto text-xxs font-normal text-black pr-2 pl-1">
-                22.
-              </div>
-              <div class="my-auto text-xxs font-normal text-black">
-                SPOUSE'S SURNAME
-              </div>
-            </div>
-            <div class="flex h-6 pl-6">
-              <div class="my-auto text-xxs font-normal text-black">
-                FIRST NAME
-              </div>
-            </div>
-            <div class="flex h-6 pl-6">
-              <div class="my-auto text-xxs font-normal text-black">
-                MIDDLE NAME
-              </div>
-            </div>
-          </div>
-          <div
-            class="border-2 border-black border-b-0 border-t-0 border-l-0 border-r-0 w-4/12"
-          >
-            <div
-              class="flex pl-3 h-6 border-2 border-t-0 border-r-0 border-black"
-            >
-              <div class="my-auto text-xxs font-normal text-black">JOHN</div>
-            </div>
-            <div
-              class="flex justify-between pl-3 h-6 border-2 border-t-0 border-r-0 border-black"
-            >
-              <div class="my-auto text-xxs font-normal text-black pr-20">
-                DOE
-              </div>
-              <div class="border-l-2 border-black bg-gray-400 pr-6">
-                <div class="my-auto text-xxs font-normal text-black">
-                  NAME EXTENSION (JR., SR)
-                </div>
-                <div class="text-xxs text-black">JR.</div>
-              </div>
-            </div>
-            <div
-              class="flex pl-3 py-2 border-2 border-t-0 border-r-0 border-black p-1"
-            >
-              <div class="my-auto text-xxs font-normal text-black">JOHN</div>
-            </div>
-          </div>
-          <div class="w-3/12 border-b-2 border-black">
-            <div
-              class="flex h-6 border-b-2 border-l-2 border-black bg-gray-300"
-            >
-              <div class="my-auto text-xxs font-normal text-black pr-1 pl-1">
-                23.
-              </div>
-              <div class="my-auto text-xxs font-normal text-black">
-                NAME of CHILDREN (Write full name and list all)
-              </div>
-            </div>
-            <div class="flex h-6 border-b-2 border-l-2 border-black">
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                SFAFAF
-              </div>
-            </div>
-            <div class="flex h-6 border-l-2 border-black">
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                FSAAAG
-              </div>
-            </div>
-          </div>
-          <div class="w-2/12 border-b-2 border-black">
-            <div
-              class="flex h-6 border-b-2 border-l-2 border-black bg-gray-300"
-            >
-              <div class="my-auto text-xxs font-normal text-black pr-1 pl-1">
-                DATE OF BIRTH (mm/dd/yyyy)
-              </div>
-            </div>
-            <div class="flex h-6 border-b-2 border-l-2 border-black">
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                SFAFAF
-              </div>
-            </div>
-            <div class="flex h-6 border-l-2 border-black">
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                FSAAAG
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="flex">
-          <div class="w-3/12 border-b-2 border-black bg-gray-300">
-            <div
-              class="flex h-6 pl-6 border-2 border-t-0 border-r-0 border-l-0 border-black"
-            >
-              <div class="my-auto text-xxs font-normal text-black">
-                OCCUPATION
-              </div>
-            </div>
-            <div
-              class="flex h-6 pl-6 border-2 border-t-0 border-r-0 border-l-0 border-black"
-            >
-              <div class="my-auto text-xxs font-normal text-black">
-                EMPLOYER/BUSINESS NAME
-              </div>
-            </div>
-            <div
-              class="flex h-6 pl-6 border-2 border-t-0 border-r-0 border-l-0 border-black"
-            >
-              <div class="my-auto text-xxs font-normal text-black">
-                BUSINESS ADDRESS
-              </div>
-            </div>
-            <div class="flex h-6 pl-6">
-              <div class="my-auto text-xxs font-normal text-black">
-                TELEPHONE NO.
-              </div>
-            </div>
-          </div>
-          <div
-            class="border-2 border-black border-b-0 border-t-0 border-l-0 border-r-0 w-4/12"
-          >
-            <div
-              class="flex pl-3 h-6 border-2 border-t-0 border-r-0 border-black"
-            >
-              <div class="my-auto text-xxs font-normal text-black">JOHN</div>
-            </div>
-            <div
-              class="flex justify-between pl-3 h-6 border-2 border-t-0 border-r-0 border-black"
-            >
-              <div class="my-auto text-xxs font-normal text-black pr-20">
-                DOE
-              </div>
-            </div>
-            <div
-              class="flex justify-between pl-3 h-6 border-2 border-t-0 border-r-0 border-black"
-            >
-              <div class="my-auto text-xxs font-normal text-black pr-20">
-                DOE
-              </div>
-            </div>
-            <div
-              class="flex pl-3 py-2 border-2 border-t-0 border-r-0 border-black p-1"
-            >
-              <div class="my-auto text-xxs font-normal text-black">JOHN</div>
-            </div>
-          </div>
-          <div class="w-3/12 border-b-2 border-black">
-            <div
-              class="flex h-6 border-b-2 border-l-2 border-black"
-            >
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                SFAF
-              </div>
-            </div>
-            <div class="flex h-6 border-b-2 border-l-2 border-black">
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                SFAFAF
-              </div>
-            </div>
-            <div class="flex h-6 border-b-2 border-l-2 border-black">
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                SFAFAF
-              </div>
-            </div>
-            <div class="flex h-6 border-l-2 border-black">
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                FSAAAG
-              </div>
-            </div>
-          </div>
-          <div class="w-2/12 border-b-2 border-black">
-            <div
-              class="flex h-6 border-b-2 border-l-2 border-black"
-            >
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                11/1111/1/1
-              </div>
-            </div>
-            <div class="flex h-6 border-b-2 border-l-2 border-black">
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                SFAFAF
-              </div>
-            </div>
-            <div class="flex h-6 border-b-2 border-l-2 border-black">
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                SFAFAF
-              </div>
-            </div>
-            <div class="flex h-6 border-l-2 border-black">
-              <div class="my-auto text-xxs font-normal text-black pl-2">
-                FSAAAG
-              </div>
-            </div>
+            III. EDUCATIONAL BACKGROUND
           </div>
         </div>
       </div>
@@ -263,6 +68,7 @@ import Layout from "@/Shared/Layout";
 import Vue from "vue";
 import VueHtmlToPaper from "vue-html-to-paper";
 import Personal from "@/Pages/Sheet/Components/Personal";
+import Family from "@/Pages/Sheet/Components/Family";
 
 const options = {
   name: "_blank",
@@ -277,6 +83,7 @@ export default {
   layout: Layout,
   components: {
     Personal,
+    Family,
   },
   methods: {
     print() {
