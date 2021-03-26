7b9b750eb1b12baecd10b1aaed90ea0fb5e45c82
Isaac <isaacdarcilla@gmail.com>
2021-03-26 13:50:30 +0800

    fix: sass loader

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,17 +5,6 @@ const path = require('path')
 const purgecss = require('@fullhuman/postcss-purgecss')
 const tailwindcss = require('tailwindcss')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/js/app.js', 'public/js')
   .vue()
   .postCss('resources/css/app.css', 'public/css/app.css')
