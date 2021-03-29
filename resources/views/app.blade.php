b23437647ec8e9102de62c364f43145eeaaf3031
Isaac <isaacdarcilla@gmail.com>
2021-03-29 09:56:06 +0800

    fix: local development (#3)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <link rel="icon" type="image/png" href="/img/user.png"/>
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{  mix('/css/app.css') }}" rel="stylesheet">
 
     {{-- Inertia --}}
     <script src="https://polyfill.io/v3/polyfill.min.js?features=smoothscroll,NodeList.prototype.forEach,Promise,Object.values,Object.assign" defer></script>
@@ -12,7 +12,7 @@
     {{-- HRM --}}
     <script src="https://polyfill.io/v3/polyfill.min.js?features=String.prototype.startsWith" defer></script>
 
-    <script src="{{ asset('/js/app.js') }}" defer></script>
+    <script src="{{ mix('/js/app.js') }}" defer></script>
     @routes
 </head>
 <body class="font-sans leading-none text-gray-700 antialiased">
